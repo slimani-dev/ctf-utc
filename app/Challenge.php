dab92710f419c8ba3a9e1c7ad3333cd8eb9ef7da
Mohamed <slimani.m1993@gmail.com>
2019-09-23 19:53:40 +0100

    ide helper file generated

--- app/Challenge.php
+++ app/Challenge.php
@@ -4,6 +4,14 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Challenge
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Challenge newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Challenge newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Challenge query()
+ * @mixin \Eloquent
+ */
 class Challenge extends Model
 {
     //
